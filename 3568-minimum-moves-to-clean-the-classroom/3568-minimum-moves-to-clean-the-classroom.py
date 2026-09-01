4bb894163a1bb3256063be8e95cb859b120fb1ff
nikshith nayak <nikshithnayak6@gmail.com>
2026-09-01 22:53:46 +0530

    Time: 3695 ms (41.67%), Space: 56.2 MB (41.67%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
@@ -7,43 +7,35 @@ class Solution(object):
         m = len(classroom)
         n = len(classroom[0])
 
+        # Find start and litter
         litter = {}
-        start_r = start_c = 0
         litter_count = 0
 
-        # Find start and all litter positions
-        for i in range(m):
-            for j in range(n):
+        sr = sc = 0
 
-                if classroom[i][j] == 'S':
-                    start_r = i
-                    start_c = j
+        for r in range(m):
+            for c in range(n):
 
-                elif classroom[i][j] == 'L':
-                    litter[(i, j)] = litter_count
+                if classroom[r][c] == 'S':
+                    sr, sc = r, c
+
+                elif classroom[r][c] == 'L':
+                    litter[(r, c)] = litter_count
                     litter_count += 1
 
-        # If there is no litter
         if litter_count == 0:
             return 0
 
-        # Mask when all litter is collected
         all_mask = (1 << litter_count) - 1
 
-        # BFS queue:
-        # (row, col, remaining_energy, collected_mask, moves)
+        # best[r][c][mask] = maximum remaining energy
+        best = {}
+
         queue = deque()
 
-        queue.append(
-            (start_r, start_c, energy, 0, 0)
-        )
+        queue.append((sr, sc, energy, 0, 0))
 
-        # Store visited states
-        visited = set()
-
-        visited.add(
-            (start_r, start_c, energy, 0)
-        )
+        best[(sr, sc, 0)] = energy
 
         directions = [
             (1, 0),
@@ -56,27 +48,24 @@ class Solution(object):
 
             r, c, remaining, mask, moves = queue.popleft()
 
-            # Try all 4 directions
             for dr, dc in directions:
 
                 nr = r + dr
                 nc = c + dc
 
-                # Boundary check
+                # Outside grid
                 if nr < 0 or nr >= m or nc < 0 or nc >= n:
                     continue
 
-                # Cannot pass obstacle
+                # Obstacle
                 if classroom[nr][nc] == 'X':
                     continue
 
-                # Moving costs 1 energy
-                new_energy = remaining - 1
-
-                # Cannot move without energy
-                if new_energy < 0:
+                # Need at least 1 energy to move
+                if remaining == 0:
                     continue
 
+                new_energy = remaining - 1
                 new_mask = mask
 
                 # Collect litter
@@ -84,7 +73,7 @@ class Solution(object):
                     bit = litter[(nr, nc)]
                     new_mask |= (1 << bit)
 
-                # Reset energy
+                # Reset area
                 if classroom[nr][nc] == 'R':
                     new_energy = energy
 
@@ -92,25 +81,18 @@ class Solution(object):
                 if new_mask == all_mask:
                     return moves + 1
 
-                state = (
-                    nr,
-                    nc,
-                    new_energy,
-                    new_mask
+                state = (nr, nc, new_mask)
+
+                # IMPORTANT OPTIMIZATION:
+                # Skip if we've already reached this state
+                # with equal or more energy
+                if state in best and best[state] >= new_energy:
+                    continue
+
+                best[state] = new_energy
+
+                queue.append(
+                    (nr, nc, new_energy, new_mask, moves + 1)
                 )
-
-                if state not in visited:
-
-                    visited.add(state)
-
-                    queue.append(
-                        (
-                            nr,
-                            nc,
-                            new_energy,
-                            new_mask,
-                            moves + 1
-                        )
-                    )
 
         return -1
